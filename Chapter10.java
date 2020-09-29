3d530f530cc3f7680bd89273e842d70224fce589
KeltonPygott <42286001+KeltonPygott@users.noreply.github.com>
2020-09-29 11:03:32 -0600

    Create Chapter10.java

---
+++ Chapter10.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+ 
+public class Average {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input first number: ");
+  int num1 = in.nextInt();
+   
+  System.out.print("Input second number: ");
+  int num2 = in.nextInt();
+   
+  System.out.print("Input third number: ");
+  int num3 = in.nextInt();
+   
+  System.out.println("Average of three numbers is: " + 
+   (num1 + num2 + num3 ) / 3);
+ }
+}
+
+// Exersice 1
+
+public class trigonometry {
+    public static void main ( String[] args )
+      {
+        double a = Math.sin(0.5236);
+        double b = Math.cos(0.5236);
+        double c = (a*a) + (b*b);
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+        System.out.println("c is " + c);
+      }
+}
+
+// Exersise 2
+
+public class trigonometry {
+    public static void main ( String[] args )
+      {
+        double a = Math.sin(0.5236);
+        double b = Math.cos(0.5236);
+        double c = (a*a) + (b*b);
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+        System.out.println("c is " + c);
+        rad = degrees * Math.PI/180
+      }
+}
+
+// Exersise 3 
+
+
+
+
