3ba0e330ebe2f8076b7e147534148d569a3aafa4
KeltonPygott <42286001+KeltonPygott@users.noreply.github.com>
2020-09-29 11:13:08 -0600

    Create Chapter11.java

---
+++ Chapter11.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+ 
+public class CreateRunTime {
+ 
+ public static void main(String[] args) {
+  Scanner RunTime = new Scanner(System.in);
+  
+  str = ("Hello World!");
+  
+  System.out.println(" Input a secondary string.) ;
+   string string1 = in.nextLine();
+   
+  System.out.println(string1)
+  {
+ {
+ 
+ // Exersise 1
+ 
+ public class StringDemo2
+{
+  public static void main ( String[] args )
+  {
+    String str;
+    int    len;
+
+    str = new String( "Elementary, my dear Watson!" );
+
+    len = str.length();
+
+    System.out.println("The length is: " + len );
+
+  }
+}
+
+// Exersise 2
+
+public class StringVariable {
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str);
+    }
+
+}
+
+// Exersise 3
+ 
+  
+  
