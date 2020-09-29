236d2fae9f4d812069322849e8a231b298a1fbf0
KeltonPygott <42286001+KeltonPygott@users.noreply.github.com>
2020-09-29 11:15:11 -0600

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,14 @@
+// FORGOT TO ADD THIS TO MY OTHER FILE. 
+
+public class Substring {
+
+    public static void main ( String[] args ) {
+    String str = new String( "Hello World" ); 
+
+    String sub = str.substring(9); 
+
+    System.out.println( sub );
+
+     }
+
+}
