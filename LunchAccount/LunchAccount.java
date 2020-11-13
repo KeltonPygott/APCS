9e4afe90f922fe754c52f26d99dbaedbdf5d2c53
KeltonPygott <42286001+KeltonPygott@users.noreply.github.com>
2020-11-13 10:32:38 -0700

    Create LunchAccount.java
    Re-code classes with syntax,, 
    Fix object errors when debugging
    Fix instances in classes.

---
+++ LunchAccount/LunchAccount.java
@@ -0,0 +1,49 @@
+import java.util.UUID;
+
+public class lunchAccount(student, id, balance,) {
+    public String student;
+    public int id;  
+    public double balance; 
+    public boolean approved;
+}
+
+ public lunchAccount() {
+     this.student
+     this.id
+     this.balance
+}
+
+public id() {
+    public static void main(String[] args) {
+    UUID id = UUID.randomUUID(4);
+    System.out.println (id);
+}
+
+public student() {
+
+}
+
+public balance() {
+
+}
+public items() {
+    
+}
+
+
+
+public static void printData(lunchAccount) {
+    Syetem.out.print(this.student)
+    System.out.print(:)
+    System.out.print(this.id)
+    System.out.print(:)
+    System.out.print(this.balance)
+
+}
+
+public class main(lunchAccount) {
+    if (balance > items) 
+    boolean approved = true 
+}
+
+
